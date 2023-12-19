76897d7d97879e347c809b5bc7ffcc71b899bf4e
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2023-12-19 13:57:13 +0300

    completed challenge

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,40 @@
+# Inside the lib/list_crud.py file
+
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
+    return [1, 2, 3, 4]
+
+def add_element_to_end_of_list(lst, element):
+    lst.append(element)
+    return lst
+
+def add_element_to_start_of_list(lst, element):
+    lst.insert(0, element)
+    return lst
+
+def remove_element_from_end_of_list(lst):
+    if lst:
+        lst.pop()
+    return lst
+
+def remove_element_from_start_of_list(lst):
+    if lst:
+        del lst[0]
+    return lst
+
+def retrieve_first_element_from_list(lst):
+    if lst:
+        return lst[0]
     return None
 
-def add_element_to_end_of_list(l, element):
+def retrieve_element_from_index(lst, index):
+    if 0 <= index < len(lst):
+        return lst[index]
     return None
 
-def add_element_to_start_of_list(l, element):
-    return None
-
-def remove_element_from_end_of_list(l):
-    return None
-
-def remove_element_from_start_of_list(l):
-    return None
-
-def retrieve_first_element_from_list(l):
-    return None
-
-def retrieve_element_from_index(l, index):
-    return None
-
-def retrieve_last_element_from_list(l):
+def retrieve_last_element_from_list(lst):
+    if lst:
+        return lst[-1]
     return None
